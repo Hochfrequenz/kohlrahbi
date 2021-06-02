157ea29e523e87b46c35affe292bc025538f8c49
Kevin <kevin.krechan@hochfrequenz.de>
2021-06-02 22:02:19 +0200

    🚧 Add three column tables

--- ahb_extractor.py
+++ ahb_extractor.py
@@ -41,38 +41,34 @@ def main():
             for row in range(len(table.rows)):
 
                 # dataframe_row = (len(df.columns)) * [np.nan]
+
+                # initial empty list for the next row in the dataframe
                 dataframe_row = (len(df.columns)) * [""]
 
+                # idea: cause of double information in 4 column tables, remove entry [2] from row_cell_texts_as_list
+                row_cell_texts_as_list = [cell.text for cell in table.row_cells(row)]
+
                 # skip table header
-                if table.row_cells(row)[0].text == "EDIFACT Struktur":
+                if row_cell_texts_as_list[0] == "EDIFACT Struktur":
                     continue
 
                 # distinguish between 4 and 3 column tables
-                if len(table.row_cells(row)) == 4:
-
-                    # for cell in table.row_cells(row):
-
-                    # breakpoint
-
-                    row_cells_as_list = [cell.text for cell in table.row_cells(row)]
-
-                    # Write segment name like "Nachrichten-Kopfsegment"
+                if len(table.row_cells(row)) == 3:
                     if (
-                        row_cells_as_list[0]
-                        and not row_cells_as_list[1]
-                        and not row_cells_as_list[2]
-                        and not row_cells_as_list[3]
+                        row_cell_texts_as_list[0]
+                        and not row_cell_texts_as_list[1]
+                        and not row_cell_texts_as_list[2]
+                        # and not row_cell_texts_as_list[3]
                     ):
-                        dataframe_row[0] = row_cells_as_list[0]
+                        dataframe_row[0] = row_cell_texts_as_list[0]
                         df.loc[df_row_index] = dataframe_row
                         df_row_index = df_row_index + 1
                         continue
 
-                    # for cell in table.row_cells(row):
-                    # middle column is double in four column tables -> skip one column
-                    for column_number in [0, 1, 3]:
+                    for column_number in [0, 1, 2]:
                         cell = table.row_cells(row)[column_number]
 
+                        # LEFT COLUMN -> 0
                         # at this left_indent, there is now Segment-Gruppe
                         if column_number == 0 and cell.paragraphs[0].paragraph_format.left_indent == 364490:
                             # Segement and Datenelement in cell, example: "UNH\t0062"
@@ -85,11 +81,12 @@ def main():
                                     splitted_edifact_struktur_column[1],
                                 )
                                 continue
-                            # only Segement in cell, example: "UNH"
+                            # only Segment in cell, example: "UNH"
                             else:
                                 dataframe_row[1] = cell.text
                                 continue
 
+                        # MIDDLE COLUMN -> 1
                         if column_number == 1:
                             # check if cell is in Segment level
                             if cell.paragraphs[0].paragraph_format.left_indent is None:
@@ -107,7 +104,8 @@ def main():
 
                                 if "\n" in cell.text:
 
-                                    if len(tabs) == 4 and "\t" in cell.text.split("\n")[1]:
+                                    # if len(tabs) == 4 and "\t" in cell.text.split("\n")[1]:
+                                    if len(tabs) == 4 and cell.text.count("\t") == 5:
                                         splitted_text_at_line_endings: List = cell.text.split("\n")
 
                                         splitted_text_at_tabs = [
@@ -121,6 +119,7 @@ def main():
 
                                         dataframe_row[3], dataframe_row[4] = fixed_text[0], fixed_text[1]
 
+                                # example: Nachrichten-Referenznummer
                                 if len(tabs) == 3:
                                     splitted_text = cell.text.split("\t")
                                     dataframe_row[3], dataframe_row[5], dataframe_row[6], dataframe_row[7] = (
@@ -128,6 +127,164 @@ def main():
                                         splitted_text[1],
                                         splitted_text[2],
                                         splitted_text[3],
+                                    )
+
+                                if len(tabs) == 4 and cell.text.count("\t") == 4 and not "\n" in cell.text:
+                                    splitted_text: List = cell.text.split("\t")
+                                    (
+                                        dataframe_row[3],
+                                        dataframe_row[4],
+                                        dataframe_row[5],
+                                        dataframe_row[6],
+                                        dataframe_row[7],
+                                    ) = (
+                                        splitted_text[0],
+                                        splitted_text[1],
+                                        splitted_text[2],
+                                        splitted_text[3],
+                                        splitted_text[4],
+                                    )
+
+                                if len(tabs) == 4 and cell.text.count("\t") == 4 and "\n" in cell.text:
+                                    splitted_text = cell.text.split("\t")
+                                    wrapped_lines = splitted_text[-1].split("\n")
+                                    splitted_text[1] = splitted_text[1] + " " + " ".join(wrapped_lines[1:])
+                                    splitted_text[4] = wrapped_lines[0]
+                                    (
+                                        dataframe_row[3],
+                                        dataframe_row[4],
+                                        dataframe_row[5],
+                                        dataframe_row[6],
+                                        dataframe_row[7],
+                                    ) = (
+                                        splitted_text[0],
+                                        splitted_text[1],
+                                        splitted_text[2],
+                                        splitted_text[3],
+                                        splitted_text[4],
+                                    )
+
+                # distinguish between 4 and 3 column tables
+                if len(table.row_cells(row)) == 4:
+
+                    # for cell in table.row_cells(row):
+
+                    # breakpoint
+                    # row_cell_texts_as_list = [cell.text for cell in table.row_cells(row)]
+
+                    # Write segment name like "Nachrichten-Kopfsegment"
+                    if (
+                        row_cell_texts_as_list[0]
+                        # and not row_cell_texts_as_list[1]
+                        and not row_cell_texts_as_list[2]
+                        and not row_cell_texts_as_list[3]
+                    ):
+                        dataframe_row[0] = row_cell_texts_as_list[0]
+                        df.loc[df_row_index] = dataframe_row
+                        df_row_index = df_row_index + 1
+                        continue
+
+                    # for cell in table.row_cells(row):
+
+                    # ----------------
+                    # middle column is double in four column tables -> skip one column
+                    for column_number in [0, 1, 3]:
+                        cell = table.row_cells(row)[column_number]
+
+                        # LEFT COLUMN -> 0
+                        # at this left_indent, there is now Segment-Gruppe
+                        if column_number == 0 and cell.paragraphs[0].paragraph_format.left_indent == 364490:
+                            # Segement and Datenelement in cell, example: "UNH\t0062"
+                            if "\t" in cell.text:
+                                splitted_edifact_struktur_column: List = cell.text.split("\t")
+                                print(splitted_edifact_struktur_column)
+                                # TODO if someone knows a better way, suggestions are very welcome =)
+                                dataframe_row[1], dataframe_row[2] = (
+                                    splitted_edifact_struktur_column[0],
+                                    splitted_edifact_struktur_column[1],
+                                )
+                                continue
+                            # only Segment in cell, example: "UNH"
+                            else:
+                                dataframe_row[1] = cell.text
+                                continue
+
+                        # MIDDLE COLUMN -> 1
+                        if column_number == 1:
+                            # check if cell is in Segment level
+                            if cell.paragraphs[0].paragraph_format.left_indent is None:
+                                splitted_text = cell.text.split("\t")
+                                dataframe_row[5], dataframe_row[6], dataframe_row[7] = (
+                                    splitted_text[1],
+                                    splitted_text[2],
+                                    splitted_text[3],
+                                )
+
+                            # check if cell is in Datenelement level
+                            if cell.paragraphs[0].paragraph_format.left_indent == 36830:
+
+                                tabs = cell.paragraphs[0].paragraph_format.tab_stops._pPr.tabs
+
+                                if "\n" in cell.text:
+
+                                    # if len(tabs) == 4 and "\t" in cell.text.split("\n")[1]:
+                                    if len(tabs) == 4 and cell.text.count("\t") == 5:
+                                        splitted_text_at_line_endings: List = cell.text.split("\n")
+
+                                        splitted_text_at_tabs = [
+                                            snippet.split("\t") for snippet in splitted_text_at_line_endings
+                                        ]
+
+                                        fixed_text = [
+                                            text[0] + text[1]
+                                            for text in zip(splitted_text_at_tabs[0], splitted_text_at_tabs[1])
+                                        ]
+
+                                        dataframe_row[3], dataframe_row[4] = fixed_text[0], fixed_text[1]
+
+                                # example: Nachrichten-Referenznummer
+                                if len(tabs) == 3:
+                                    splitted_text = cell.text.split("\t")
+                                    dataframe_row[3], dataframe_row[5], dataframe_row[6], dataframe_row[7] = (
+                                        splitted_text[0],
+                                        splitted_text[1],
+                                        splitted_text[2],
+                                        splitted_text[3],
+                                    )
+
+                                if len(tabs) == 4 and cell.text.count("\t") == 4 and not "\n" in cell.text:
+                                    splitted_text: List = cell.text.split("\t")
+                                    (
+                                        dataframe_row[3],
+                                        dataframe_row[4],
+                                        dataframe_row[5],
+                                        dataframe_row[6],
+                                        dataframe_row[7],
+                                    ) = (
+                                        splitted_text[0],
+                                        splitted_text[1],
+                                        splitted_text[2],
+                                        splitted_text[3],
+                                        splitted_text[4],
+                                    )
+
+                                if len(tabs) == 4 and cell.text.count("\t") == 4 and "\n" in cell.text:
+                                    splitted_text = cell.text.split("\t")
+                                    wrapped_lines = splitted_text[-1].split("\n")
+                                    splitted_text[1] = splitted_text[1] + " " + " ".join(wrapped_lines[1:])
+                                    splitted_text[4] = wrapped_lines[0]
+                                    (
+                                        dataframe_row[3],
+                                        dataframe_row[4],
+                                        dataframe_row[5],
+                                        dataframe_row[6],
+                                        dataframe_row[7],
+                                    ) = (
+                                        splitted_text[0],
+                                        splitted_text[1],
+                                        splitted_text[2],
+                                        splitted_text[3],
+                                        splitted_text[4],
                                     )
 
                                 # Beschreibung -> 4 -> 436245
@@ -140,52 +297,52 @@ def main():
                                 print(cell.text)
                                 print(cell.paragraphs[0].paragraph_format.left_indent)
 
-                    df.loc[df_row_index] = dataframe_row
+                df.loc[df_row_index] = dataframe_row
 
-                    df_row_index = df_row_index + 1
+                df_row_index = df_row_index + 1
 
-                    # if cell.paragraphs[0].paragraph_format.left_indent == 364490:
+                # if cell.paragraphs[0].paragraph_format.left_indent == 364490:
 
-                    # Write in column: Segement-Gruppe
-                    # 'SG2'
-                    # cell.paragraphs[0].paragraph_format.left_indent -> 36830
-                    # 'SG2\tNAD'
-                    # 36830
-                    # '\tMuss\tMuss\tMuss'
-                    # None
-                    # ''
-                    # None
-                    # ↩
-                    # 'SG2\tNAD\t3035'
-                    # 36830
+                # Write in column: Segement-Gruppe
+                # 'SG2'
+                # cell.paragraphs[0].paragraph_format.left_indent -> 36830
+                # 'SG2\tNAD'
+                # 36830
+                # '\tMuss\tMuss\tMuss'
+                # None
+                # ''
+                # None
+                # ↩
+                # 'SG2\tNAD\t3035'
+                # 36830
 
-                    # Write in column: Segment
-                    # 'UNH'
-                    # cell.paragraphs[0].paragraph_format.left_indent -> 364490
+                # Write in column: Segment
+                # 'UNH'
+                # cell.paragraphs[0].paragraph_format.left_indent -> 364490
 
-                    print(repr(cell.text))
-                    print(cell.paragraphs[0].paragraph_format.left_indent)
-                    # dataframe_row[0] = repr(cell.text)
+                print(repr(cell.text))
+                print(cell.paragraphs[0].paragraph_format.left_indent)
+                # dataframe_row[0] = repr(cell.text)
 
-                    # print(cell.paragraphs[0].paragraph_format.tab_stops)
-                    # print(cell.paragraphs[0].paragraph_format.tab_stops._pPr)
-                    # print(cell.paragraphs[0].paragraph_format.tab_stops._pPr.tabs)
+                # print(cell.paragraphs[0].paragraph_format.tab_stops)
+                # print(cell.paragraphs[0].paragraph_format.tab_stops._pPr)
+                # print(cell.paragraphs[0].paragraph_format.tab_stops._pPr.tabs)
 
-                    # tabs = cell.paragraphs[0].paragraph_format.tab_stops._pPr.tabs
-                    # if tabs is not None:
+                # tabs = cell.paragraphs[0].paragraph_format.tab_stops._pPr.tabs
+                # if tabs is not None:
 
-                    #     # tab_position_list = (len(df.columns) - 1) * [np.nan]
-                    #     i = 1
-                    #     for tab in tabs:
-                    #         # print(tab.pos)
+                #     # tab_position_list = (len(df.columns) - 1) * [np.nan]
+                #     i = 1
+                #     for tab in tabs:
+                #         # print(tab.pos)
 
-                    #         dataframe_row[i] = tab.pos
-                    #         i = i + 1
-                    #         # dataframe_row.append(tab_position_list)
+                #         dataframe_row[i] = tab.pos
+                #         i = i + 1
+                #         # dataframe_row.append(tab_position_list)
 
-                    # df.loc[df_row_index] = dataframe_row
+                # df.loc[df_row_index] = dataframe_row
 
-                    # df_row_index = df_row_index + 1
+                # df_row_index = df_row_index + 1
 
                 # print(tabs.tag)
                 # print(tabs.attrib)
