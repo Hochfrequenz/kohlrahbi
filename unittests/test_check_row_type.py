dff085ba7e0882a9a2d92510472f71fcf4181072
Kevin <kevin.krechan@hochfrequenz.de>
2021-06-11 21:20:58 +0200

    ✅🎨 Restructure test for check_row_type

--- unittests/test_check_row_type.py
+++ unittests/test_check_row_type.py
@@ -6,24 +6,65 @@ from check_row_type import RowType, define_row_type
 
 
 class TestCheckRowType:
+
+    # this is the touchstone position for the left indent of the edifact struktur cells
+    # example rows with this left indent position:
+    # |      UNH       |
+    # |      UNH\t0062 |
+    segment_left_indent_position = 364490
+
+    # example rows with this left indent position:
+    # | SG2            |
+    # | SG2\tNAD       |
+    # | SG2\tNAD\t3025 |
+    segmentgruppe_left_indent_position = 36830
+
     @pytest.mark.parametrize(
         "text, left_indent_position, font_color, expected",
         [
-            pytest.param("EDIFACT Struktur", 1905, RGBColor(0, 0, 0), RowType.HEADER, id="HEADER"),
             pytest.param(
-                "Nachrichten-Kopfsegment", 1270, RGBColor(128, 128, 128), RowType.SEGMENTNAME, id="SEGMENTNAME"
-            ),
-            pytest.param("SG2", 1270, RGBColor(0, 0, 0), RowType.SEGMENTGRUPPE, id="SEGMENTGRUPPE"),
-            pytest.param("UNH", 3810, RGBColor(0, 0, 0), RowType.SEGMENT, id="SEGMENT w/o Segmentgruppe"),
-            pytest.param("SG2\tNAD", 1270, RGBColor(0, 0, 0), RowType.SEGMENT, id="SEGMENT w Segementgruppe"),
-            pytest.param(
-                "UNH\t0062", 3810, RGBColor(0, 0, 0), RowType.DATENELEMENT, id="DATENELEMENT w/o Segmentgruppe"
+                "EDIFACT Struktur", segmentgruppe_left_indent_position, RGBColor(0, 0, 0), RowType.HEADER, id="HEADER"
             ),
             pytest.param(
-                "SG2\tNAD\t3035", 1270, RGBColor(0, 0, 0), RowType.DATENELEMENT, id="DATENELEMENT w Segementgruppe"
+                "Nachrichten-Kopfsegment",
+                segmentgruppe_left_indent_position,
+                RGBColor(128, 128, 128),
+                RowType.SEGMENTNAME,
+                id="SEGMENTNAME",
+            ),
+            pytest.param(
+                "SG2", segmentgruppe_left_indent_position, RGBColor(0, 0, 0), RowType.SEGMENTGRUPPE, id="SEGMENTGRUPPE"
+            ),
+            pytest.param(
+                "UNH",
+                segment_left_indent_position,
+                RGBColor(0, 0, 0),
+                RowType.SEGMENT,
+                id="SEGMENT w/o Segmentgruppe",
+            ),
+            pytest.param(
+                "SG2\tNAD",
+                segmentgruppe_left_indent_position,
+                RGBColor(0, 0, 0),
+                RowType.SEGMENT,
+                id="SEGMENT w Segementgruppe",
+            ),
+            pytest.param(
+                "UNH\t0062",
+                segment_left_indent_position,
+                RGBColor(0, 0, 0),
+                RowType.DATENELEMENT,
+                id="DATENELEMENT w/o Segmentgruppe",
+            ),
+            pytest.param(
+                "SG2\tNAD\t3035",
+                segmentgruppe_left_indent_position,
+                RGBColor(0, 0, 0),
+                RowType.DATENELEMENT,
+                id="DATENELEMENT w Segementgruppe",
             ),
             pytest.param("", 635, RGBColor(0, 0, 0), RowType.EMPTY, id="EMPTY"),
-        ],
+        ],  # TODO find left indent for empty cell
     )
     def test_define_row_type(self, text: str, left_indent_position: int, font_color: RGBColor, expected: RowType):
         """Test if all defined row types are identified correctly.
@@ -35,9 +76,9 @@ class TestCheckRowType:
             expected (RowType): The expected RowType for each test case
         """
         # ! Attention: It seems that you can set the left indent to discret numbers!
-        edifact_struktur_left_indent_position = 1270
+        # edifact_struktur_left_indent_position = 1270
 
-        # create table test cell
+        # create table test cell, it contains per default an empty paragraph
         test_document = docx.Document()
         test_table = test_document.add_table(rows=1, cols=1)
         test_cell = test_table.add_row().cells[0]
@@ -51,6 +92,6 @@ class TestCheckRowType:
         test_cell.paragraphs[0].runs[0].font.color.rgb = font_color
 
         result = define_row_type(
-            edifact_struktur_cell=test_cell, left_indent_position=edifact_struktur_left_indent_position
+            edifact_struktur_cell=test_cell, left_indent_position=self.segment_left_indent_position
         )
         assert result == expected
