323aa70daae5882da248bacb92fbaaefbdceb564
Kevin <kevin.krechan@hochfrequenz.de>
2021-06-14 18:07:25 +0200

    ✅🎨 Move some class variables into function to avoid conflicts
    during tests which caused failing tests

--- unittests/test_write_functions.py
+++ unittests/test_write_functions.py
@@ -425,15 +425,6 @@ class _Paragraph:
 
 class TestWriteFunctions:
 
-    # create table test cell
-    # it contains per default an empty paragraph
-    test_document = docx.Document()
-    test_table = test_document.add_table(rows=1, cols=3)
-
-    edifact_struktur_cell = test_table.add_row().cells[0]
-    middle_cell = test_table.add_row().cells[1]
-    bedingung_cell = test_table.add_row().cells[2]
-
     # this left indent and tabstop positions are equal to
     # the left indent and tabstop positions of the indicator paragraph
     middle_cell_left_indent_position_of_indicator_paragraph = 36830
@@ -508,6 +499,62 @@ class TestWriteFunctions:
         row_cells,
         expected_df_row,
     ):
+        # create table test cell
+        # it contains per default an empty paragraph
+        test_document = docx.Document()
+        test_table = test_document.add_table(rows=1, cols=3)
+
+        self.edifact_struktur_cell = test_table.add_row().cells[0]
+        self.middle_cell = test_table.add_row().cells[1]
+        self.bedingung_cell = test_table.add_row().cells[2]
+
+        # prepare edifact struktur cell
+        current_paragraph = self.edifact_struktur_cell.paragraphs[0]
+
+        current_paragraph.text = row_cells["edifact_struktur_cell"][0].text
+        current_paragraph.paragraph_format.left_indent = row_cells["edifact_struktur_cell"][0].left_indent_position
+
+        if row_cells["edifact_struktur_cell"][0].tabstop_positions is not None:
+            for tabstop_position in row_cells["edifact_struktur_cell"][0].tabstop_positions:
+                self.edifact_struktur_cell.paragraphs[0].paragraph_format.tab_stops.add_tab_stop(tabstop_position)
+
+        for _paragraph, i in zip(
+            row_cells["edifact_struktur_cell"][1:], range(1, len(row_cells["edifact_struktur_cell"]))
+        ):
+            self.edifact_struktur_cell.add_paragraph()
+            current_paragraph = self.edifact_struktur_cell.paragraphs[i]
+            current_paragraph.text = _paragraph.text
+
+            current_paragraph.paragraph_format.left_indent = _paragraph.left_indent_position
+            current_tab_stops = current_paragraph.paragraph_format.tab_stops
+
+            if _paragraph.tabstop_positions is not None:
+                for tabstop_position in _paragraph.tabstop_positions:
+                    current_tab_stops.add_tab_stop(tabstop_position)
+
+        # Initial two dataframes ...
+        df = pd.DataFrame(columns=expected_df_row.keys(), dtype="str")
+        expected_df = pd.DataFrame(columns=expected_df_row.keys(), dtype="str")
+        row_index = 0
+        # ... with a row full of emtpy strings
+        initial_dataframe_row = (len(df.columns)) * [""]
+        df.loc[row_index] = initial_dataframe_row
+        expected_df.loc[row_index] = initial_dataframe_row
+
+        write_segment_name_to_dataframe(
+            dataframe=df,
+            row_index=row_index,
+            edifact_struktur_cell=self.edifact_struktur_cell,
+            edifact_struktur_cell_left_indent_position=self.edifact_struktur_cell_left_indent_position_of_indicator_paragraph,
+            middle_cell=self.middle_cell,
+            middle_cell_left_indent_position=self.middle_cell_left_indent_position_of_indicator_paragraph,
+            tabstop_positions=self.middle_cell_tabstop_positions_of_indicator_paragraph,
+            bedingung_cell=self.bedingung_cell,
+        )
+
+        expected_df.loc[row_index] = expected_df_row
+
+        assert expected_df.equals(df)
 
         # prepare edifact struktur cell
         current_paragraph = self.edifact_struktur_cell.paragraphs[0]
