0b5aba7af3da110c6d7c85fc79efb2fe3c3b34d5
Kevin <kevin.krechan@hochfrequenz.de>
2021-06-16 14:26:00 +0200

    📝Add docstrings to functions in check_row_type.py

--- AHB_Extractor/helper/check_row_type.py
+++ AHB_Extractor/helper/check_row_type.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 from docx.shared import RGBColor
+from docx.table import _Cell
 
 
 class RowType(Enum):
@@ -12,16 +13,30 @@ class RowType(Enum):
     EMPTY = 6
 
 
-def is_row_header(edifact_struktur_cell) -> bool:
+def is_row_header(edifact_struktur_cell: _Cell) -> bool:
+    """Checks if the current row is a header
+
+    Args:
+        edifact_struktur_cell (_Cell): Indicator cell
+
+    Returns:
+        bool:
+    """
     if edifact_struktur_cell.text == "EDIFACT Struktur":
         return True
 
     return False
 
 
-def is_row_segmentname(edifact_struktur_cell) -> bool:
-    """
-    Checks if the actual row contains just the segment name like "Nachrichten-Kopfsegment"
+def is_row_segmentname(edifact_struktur_cell: _Cell) -> bool:
+    """Checks if the current row contains just a segment name.
+       Example: "Nachrichten-Kopfsegment"
+
+    Args:
+        edifact_struktur_cell (_Cell): Indicator cell
+
+    Returns:
+        bool:
     """
     try:
         if edifact_struktur_cell.paragraphs[0].runs[0].font.color.rgb == RGBColor(128, 128, 128):
@@ -32,7 +47,17 @@ def is_row_segmentname(edifact_struktur_cell) -> bool:
     return False
 
 
-def is_row_segmentgruppe(edifact_struktur_cell, left_indent_position: int) -> bool:
+def is_row_segmentgruppe(edifact_struktur_cell: _Cell, left_indent_position: int) -> bool:
+    """Checks if the current row is a segmentgruppe.
+       Example: "SG2"
+
+    Args:
+        edifact_struktur_cell (_Cell): Indicator cell
+        left_indent_position (int): Position of the left indent
+
+    Returns:
+        bool:
+    """
     if (
         not edifact_struktur_cell.paragraphs[0].paragraph_format.left_indent == left_indent_position
         and not "\t" in edifact_struktur_cell.text
@@ -43,7 +68,17 @@ def is_row_segmentgruppe(edifact_struktur_cell, left_indent_position: int) -> bo
     return False
 
 
-def is_row_segment(edifact_struktur_cell, left_indent_position: int) -> bool:
+def is_row_segment(edifact_struktur_cell: _Cell, left_indent_position: int) -> bool:
+    """Checks if the current row is a segmentgruppe.
+       Example: "UNH", "SG2\tNAD"
+
+    Args:
+        edifact_struktur_cell (_Cell): Indicator cell
+        left_indent_position (int): Position of the left indent
+
+    Returns:
+        bool:
+    """
     # |   UNH    |
     if (
         edifact_struktur_cell.paragraphs[0].paragraph_format.left_indent == left_indent_position
@@ -62,7 +97,17 @@ def is_row_segment(edifact_struktur_cell, left_indent_position: int) -> bool:
     return False
 
 
-def is_row_datenelement(edifact_struktur_cell, left_indent_position: int) -> bool:
+def is_row_datenelement(edifact_struktur_cell: _Cell, left_indent_position: int) -> bool:
+    """Checks if the current row is a datenelement.
+       Example: "UNH\t00062", "SG2\tNAD\t3035"
+
+    Args:
+        edifact_struktur_cell (_Cell): Indicator cell
+        left_indent_position (int): Position of the left indent
+
+    Returns:
+        bool:
+    """
     # |   UNH\t0062 |
     if (
         edifact_struktur_cell.paragraphs[0].paragraph_format.left_indent == left_indent_position
@@ -80,13 +125,33 @@ def is_row_datenelement(edifact_struktur_cell, left_indent_position: int) -> boo
     return False
 
 
-def is_row_empty(edifact_struktur_cell) -> bool:
+def is_row_empty(edifact_struktur_cell: _Cell) -> bool:
+    """Checks if the current row is empty.
+       Example: ""
+    Args:
+        edifact_struktur_cell (_Cell): Indicator cell
+
+    Returns:
+        bool:
+    """
     if edifact_struktur_cell.text == "":
         return True
     return False
 
 
-def define_row_type(edifact_struktur_cell, left_indent_position: int) -> RowType:
+def define_row_type(edifact_struktur_cell: _Cell, left_indent_position: int) -> RowType:
+    """Defines the type of the current row.
+
+    Args:
+        edifact_struktur_cell (_Cell): Indicator cell
+        left_indent_position (int): Position of the left indent
+
+    Raises:
+        NotImplemented: Gets raised if the RowType got not be defined
+
+    Returns:
+        RowType: Type of the current row
+    """
     if is_row_header(edifact_struktur_cell=edifact_struktur_cell):
         return RowType.HEADER
 
