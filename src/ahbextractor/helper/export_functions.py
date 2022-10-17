1346af6d23c196b6091aeb53dfe54ac62bbdd9af
konstantin <konstantin.klein@hochfrequenz.de>
2022-10-17 10:54:58 +0200

    ✔ Add Unittest for `get_all_paragraphs_and_tables` (#42)

--- src/ahbextractor/helper/export_functions.py
+++ src/ahbextractor/helper/export_functions.py
@@ -11,7 +11,7 @@ save all Prüfidentifikators of one AHB in one Excel file.
 
 import re
 from pathlib import Path
-from typing import Any, Dict, TypeVar, overload, Union
+from typing import Any, Dict, TypeVar, Union, overload
 
 import pandas as pd  # type:ignore[import]
 
---
+++ unittests/test_docx_extensions.py
@@ -0,0 +1,32 @@
+"""
+tests all the features the ahbextractor package provides to process Docx files (by using the docx package)
+"""
+import pytest  # type:ignore[import]
+from _pytest.fixtures import SubRequest  # type:ignore[import]
+from docx import Document  # type:ignore[import]
+from docx.table import Table  # type:ignore[import]
+from docx.text.paragraph import Paragraph  # type:ignore[import]
+
+from ahbextractor.helper.read_functions import get_all_paragraphs_and_tables
+
+
+class TestDocxExtensions:
+    @pytest.fixture
+    def create_docx_from_filename(self, request: SubRequest, datafiles) -> Document:
+        """a fixture to quickly instantiate a docx.Document from a given docx file name"""
+        docx_file_name = request.param
+        docx_file_path = datafiles / docx_file_name
+        document = Document(docx_file_path)
+        yield document
+
+    @pytest.mark.parametrize(
+        "create_docx_from_filename",
+        [pytest.param("UTILMD_AHB_WiM-informatorischeLesefassung_3.1e_99991231_20221001.docx")],
+        indirect=True,
+    )
+    @pytest.mark.parametrize("expected_length", [pytest.param(1218)])
+    @pytest.mark.datafiles("unittests/docx_files/UTILMD_AHB_WiM-informatorischeLesefassung_3.1e_99991231_20221001.docx")
+    def test_get_all_paragraphs_and_tables(self, create_docx_from_filename: Document, expected_length: int):
+        actual = list(get_all_paragraphs_and_tables(create_docx_from_filename))
+        assert len(actual) == expected_length
+        assert all([isinstance(x, Table) or isinstance(x, Paragraph) for x in actual]) is True
