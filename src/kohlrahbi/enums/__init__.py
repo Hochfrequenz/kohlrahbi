d01e7b1ba95735e6999676706a3d230e9134dd58
kevin <kevin.krechan@hochfrequenz.de>
2022-12-10 16:14:28 +0100

    🎨 Use the new enums in kohlrahbi

---
+++ src/kohlrahbi/enums/__init__.py
@@ -0,0 +1,2 @@
+from .row_type import RowType
+from .row_type_color import RowTypeColor
---
+++ src/kohlrahbi/enums/row_type_color.py
@@ -0,0 +1,14 @@
+from enum import StrEnum
+
+
+class RowTypeColor(StrEnum):
+    """
+    Colors for each RowType
+    """
+
+    HEADER = "de324c"  #: e.g. "EDIFACT Struktur"
+    SEGMENTNAME = "f4895f"  #: e.g. "Nachrichten-Kopfsegment"
+    SEGMENTGRUPPE = "f8e16f"  #: e.g. "SG2"
+    SEGMENT = "95cf92"  #: e.g. "    UNH" or "SG2  NAD"
+    DATENELEMENT = "369acc"  #: e.g. "    UNH 0062"
+    EMPTY = "9656a2"  #: e.g. ""
