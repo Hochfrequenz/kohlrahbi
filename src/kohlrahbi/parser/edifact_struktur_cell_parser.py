6908c9cea6b495972f5cb118dbb655c9d77fd682
kevin <kevin.krechan@hochfrequenz.de>
2022-12-10 22:36:39 +0100

    🎨 Use Cell instead of paragraphs in edifact-struktur-parser

--- src/kohlrahbi/parser/edifact_struktur_cell_parser.py
+++ src/kohlrahbi/parser/edifact_struktur_cell_parser.py
@@ -1,11 +1,9 @@
-from typing import List
-
 import pandas as pd  # type:ignore[import]
-from docx.text.paragraph import Paragraph  # type:ignore[import]
+from docx.table import _Cell  # type:ignore[import]
 
 
 def parse_edifact_struktur_cell(
-    paragraphs: List[Paragraph],
+    table_cell: _Cell,
     dataframe: pd.DataFrame,
     row_index: int,
     edifact_struktur_cell_left_indent_position: int,
@@ -13,18 +11,19 @@ def parse_edifact_struktur_cell(
     """Parses a paragraph in the edifact struktur column and puts the information into the appropriate columns
 
     Args:
-        paragraphs (Paragraph): Current paragraphs in the edifact struktur cell
+        table_cell (Cell): edifact struktur cell
         dataframe (pd.DataFrame): Contains all infos
         row_index (int): Current index of the DataFrame
         edifact_struktur_cell_left_indent_position (int): Position of the left indent from the indicator edifact
             struktur cell
     """
-    joined_text = " ".join(p.text for p in paragraphs)
+
+    joined_text = " ".join(p.text for p in table_cell.paragraphs)
     splitted_text_at_tabs = joined_text.split("\t")
     tab_count = joined_text.count("\t")
 
     # Check if the line starts on the far left
-    if paragraphs[0].paragraph_format.left_indent != edifact_struktur_cell_left_indent_position:
+    if table_cell.paragraphs[0].paragraph_format.left_indent != edifact_struktur_cell_left_indent_position:
 
         if tab_count == 2:
             dataframe.at[row_index, "Segment Gruppe"] = splitted_text_at_tabs[0]
@@ -34,7 +33,7 @@ def parse_edifact_struktur_cell(
             dataframe.at[row_index, "Segment Gruppe"] = splitted_text_at_tabs[0]
             dataframe.at[row_index, "Segment"] = splitted_text_at_tabs[1]
         elif tab_count == 0 and joined_text.strip() != "":
-            if paragraphs[0].runs[0].bold:
+            if table_cell.paragraphs[0].runs[0].bold:
                 # Segmentgruppe: SG8
                 dataframe.at[row_index, "Segment Gruppe"] = splitted_text_at_tabs[0]
             else:
