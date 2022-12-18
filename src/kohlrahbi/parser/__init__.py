7866d60ae4b2edf4cd1f159cdf2604a8f2a4a87b
kevin <kevin.krechan@hochfrequenz.de>
2022-12-18 23:14:41 +0100

    🔥 Delete old parser functions

--- src/kohlrahbi/parser/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Import all parser functions for easier importing.
-"""
-
-from .bedingung_cell_parser import parse_bedingung_cell
-from .edifact_struktur_cell_parser import parse_edifact_struktur_cell
-from .middle_cell_parser import parse_middle_cell
