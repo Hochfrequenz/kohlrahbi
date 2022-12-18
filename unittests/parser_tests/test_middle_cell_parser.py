b8d0643df78e1b0f1096ecc7d4515fb4af14c9db
kevin <kevin.krechan@hochfrequenz.de>
2022-12-18 17:36:18 +0100

    ✅ Add first unit test to test body cell

--- unittests/parser_tests/test_middle_cell_parser.py
+++ unittests/parser_tests/test_middle_cell_parser.py
@@ -1,58 +1,75 @@
 import docx
 import pytest
-from docx.shared import Inches
+from docx.shared import Inches, Twips
+import pandas as pd
 
-from kohlrahbi.parser import middle_cell_parser
+from kohlrahbi.cells import BodyCell
 
 
-class TestMiddleCellParser:
+class TestBodyCell:
     """
-    This test class collects all tests in context of the middle cell parser
+    This test class contains all tests of the BodyCell class
     """
 
     @pytest.mark.parametrize(
-        ["left_indent_position", "expected_result"],
+        ["cell_text", "expected_dataframe"],
         [
-            pytest.param(Inches(1), True, id="Paragraph contains code"),
-            pytest.param(Inches(4), False, id="Paragraph does NOT contains code"),
+            pytest.param(
+                "Nachrichten-Referenznummer\tX\tX\tX",
+                pd.DataFrame(
+                    {
+                        "Segment Gruppe": [""],
+                        "Segment": [""],
+                        "Datenelement": [""],
+                        "Codes und Qualifier": ["Nachrichten-Referenznummer"],
+                        "Beschreibung": [""],
+                        "11016": ["X"],
+                        "11017": ["X"],
+                        "11018": ["X"],
+                    }
+                ),
+                id="Nachrichten-Referenznummer",
+            ),
         ],
     )
-    def test_does_paragraph_contain_qualifier_or_code(self, left_indent_position, expected_result):
-        doc = docx.Document()
-        test_paragraph = doc.add_paragraph("\tMuss\tMuss\tMuss")
-        test_paragraph.paragraph_format.left_indent = Inches(1)
-        # though it feels weird to use Inches, stick to it! I tried to use the length class Mm()
-        # but there was always a difference between:
-        #   absolute_length = Mm(1)
-        #   test_paragraph.paragraph_format.left_indent = Mm(1)
-        #   absolute_length != test_paragraph.paragraph_format.left_indent
-        # but I don't know why ...
+    def test_body_cell_parse(self, cell_text: str, expected_dataframe: pd.DataFrame):
 
-        assert (
-            middle_cell_parser.does_paragraph_contain_qualifier_or_code(
-                paragraph=test_paragraph, left_indent_position=left_indent_position
-            )
-            is expected_result
+        indicator_tabstop_positions: list[int] = [440055, 1960880, 2580640, 3191510]
+
+        indicator_tabstop_positions_in_twips = []
+
+        for position in indicator_tabstop_positions:
+            indicator_tabstop_positions_in_twips.append(Twips(position / 635))
+
+        doc = docx.Document()
+        table = doc.add_table(rows=1, cols=1)
+        body_cell = table.rows[0].cells
+        body_cell[0].text = "Nachrichten-Referenznummer\tX\tX\tX"
+
+        for tabstop_position in indicator_tabstop_positions_in_twips[1:]:
+            body_cell[0].paragraphs[0].paragraph_format.tab_stops.add_tab_stop(tabstop_position)
+
+        left_indent_length = Twips(693)
+        body_cell[0].paragraphs[0].paragraph_format.left_indent = left_indent_length
+
+        bc: BodyCell = BodyCell(
+            table_cell=table.row_cells(0)[0],
+            left_indent_position=left_indent_length,
+            indicator_tabstop_positions=indicator_tabstop_positions,
         )
 
-    def test_has_paragraph_tabstops(self):
-        doc = docx.Document()
-        test_paragraph = doc.add_paragraph("\tMuss\tMuss\tMuss")
-        test_paragraph.paragraph_format.tab_stops.add_tab_stop(Inches(0.5))
+        df: pd.DataFrame = pd.DataFrame(
+            {
+                "Segment Gruppe": [""],
+                "Segment": [""],
+                "Datenelement": [""],
+                "Codes und Qualifier": [""],
+                "Beschreibung": [""],
+                "11016": [""],
+                "11017": [""],
+                "11018": [""],
+            }
+        )
+        df = bc.parse(ahb_row_dataframe=df)
 
-        assert middle_cell_parser.has_paragraph_tabstops(paragraph=test_paragraph)
-
-    def test_parse_middle_cell(self):
-        records = ((3, "101", "Spam"), (7, "422", "Eggs"), (4, "631", "Spam, spam, eggs, and spam"))
-
-        doc = docx.Document()
-        table = doc.add_table(rows=1, cols=3)
-        hdr_cells = table.rows[0].cells
-        hdr_cells[0].text = "Qty"
-        hdr_cells[1].text = "Id"
-        hdr_cells[2].text = "Desc"
-        for qty, id, desc in records:
-            row_cells = table.add_row().cells
-            row_cells[0].text = str(qty)
-            row_cells[1].text = id
-            row_cells[2].text = desc
+        assert df.equals(expected_dataframe)
