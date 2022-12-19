ce3dfbd8406065da4493aa03d1bdbb01b3768d3c
kevin <kevin.krechan@hochfrequenz.de>
2022-12-19 10:55:35 +0100

    ✅ Write test for multi paragraph body cells

--- unittests/conftest.py
+++ unittests/conftest.py
@@ -3,24 +3,13 @@ from docx.shared import Length, Twips
 import docx
 import pandas as pd
 
-left_indent_length: Length = Twips(693)
+from unittests.bodycellparagraph import BodyCellParagraph
 
-empty_ahb_row: pd.DataFrame = pd.DataFrame(
-    {
-        "Segment Gruppe": [""],
-        "Segment": [""],
-        "Datenelement": [""],
-        "Codes und Qualifier": [""],
-        "Beschreibung": [""],
-        "11016": [""],
-        "11017": [""],
-        "11018": [""],
-    }
-)
+left_indent_length: Length = Twips(693)
 
 
 @pytest.fixture
-def create_ahb_table():
+def get_ahb_table_with_single_paragraph():
     def _setup_ahb_table(cell_text: str, tabstop_positions: list[Length]):
 
         doc = docx.Document()
@@ -33,6 +22,42 @@ def create_ahb_table():
             body_cell[0].paragraphs[0].paragraph_format.tab_stops.add_tab_stop(tabstop_position)
 
         body_cell[0].paragraphs[0].paragraph_format.left_indent = left_indent_length
+
+        return table
+
+    return _setup_ahb_table
+
+
+@pytest.fixture
+def get_ahb_table_with_multiple_paragraphs():
+    def _setup_ahb_table(body_cell_paragraphs: list[BodyCellParagraph]):
+
+        doc = docx.Document()
+        table = doc.add_table(rows=1, cols=1)
+
+        body_cell = table.rows[0].cells[0]
+
+        # the cell comes with an empty paragraph which I could not delete.
+        # So we insert the BodyCellParagraph attributes into the empty paragraph
+        first_body_cell_paragprah: BodyCellParagraph = body_cell_paragraphs[0]
+
+        body_cell.paragraphs[0].text = first_body_cell_paragprah.text
+
+        for tabstop_position in first_body_cell_paragprah.tabstop_positions:
+            body_cell.paragraphs[0].paragraph_format.tab_stops.add_tab_stop(tabstop_position)
+
+        body_cell.paragraphs[0].paragraph_format.left_indent = first_body_cell_paragprah.left_indent_length
+
+        for paragraph_index, bcp in zip(range(1, len(body_cell_paragraphs[1:]) + 1), body_cell_paragraphs[1:]):
+            # add paragraph with text
+            body_cell.add_paragraph(text=bcp.text)
+
+            # set tabstop positions
+            for tabstop_position in bcp.tabstop_positions:
+                body_cell.paragraphs[paragraph_index].paragraph_format.tab_stops.add_tab_stop(tabstop_position)
+
+            # set left indent lenght
+            body_cell.paragraphs[paragraph_index].paragraph_format.left_indent = left_indent_length
 
         return table
 
--- unittests/test_cells/test_body_cell_parser.py
+++ unittests/test_cells/test_body_cell_parser.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from kohlrahbi.cells import BodyCell
 from unittests.conftest import left_indent_length
-from unittests.conftest import empty_ahb_row
+from unittests.bodycellparagraph import BodyCellParagraph
 
 
 class TestBodyCell:
@@ -103,11 +103,28 @@ class TestBodyCell:
             ),
         ],
     )
-    def test_body_cell_parse(
-        self, create_ahb_table, cell_text: str, tabstop_positions: list[Twips], expected_dataframe: pd.DataFrame
+    def test_body_cell_parse_with_one_line(
+        self,
+        get_ahb_table_with_single_paragraph,
+        cell_text: str,
+        tabstop_positions: list[Twips],
+        expected_dataframe: pd.DataFrame,
     ):
 
-        table = create_ahb_table(cell_text, tabstop_positions)
+        empty_ahb_row: pd.DataFrame = pd.DataFrame(
+            {
+                "Segment Gruppe": [""],
+                "Segment": [""],
+                "Datenelement": [""],
+                "Codes und Qualifier": [""],
+                "Beschreibung": [""],
+                "11016": [""],
+                "11017": [""],
+                "11018": [""],
+            }
+        )
+
+        table = get_ahb_table_with_single_paragraph(cell_text, tabstop_positions)
 
         indicator_tabstop_positions: list[int] = [Twips(693), Twips(3088), Twips(4064), Twips(5026)]
 
@@ -120,3 +137,64 @@ class TestBodyCell:
         df = bc.parse(ahb_row_dataframe=empty_ahb_row)
 
         assert df.equals(expected_dataframe)
+
+    def test_body_cell_parse_with_multiple_lines(self, get_ahb_table_with_multiple_paragraphs):
+        expexted_df = pd.DataFrame(
+            {
+                "Segment Gruppe": ["", "", ""],
+                "Segment": ["", "", ""],
+                "Datenelement": ["", "", ""],
+                "Codes und Qualifier": ["EM", "FX", "TE"],
+                "Beschreibung": ["Elektronische Post", "Telefax", "Telefon"],
+                "11016": ["X [1P0..1]", "X [1P0..1]", "X [1P0..1]"],
+                "11017": ["X [1P0..1]", "X [1P0..1]", "X [1P0..1]"],
+                "11018": ["X [1P0..1]", "X [1P0..1]", "X [1P0..1]"],
+            }
+        )
+
+        body_cell_paragraphs: list[BodyCellParagraph] = [
+            BodyCellParagraph(
+                text="EM\tElektronische Post\tX [1P0..1]\tX [1P0..1]\tX [1P0..1]",
+                tabstop_positions=[Twips(693), Twips(3088), Twips(4064), Twips(5026)],
+                left_indent_length=Twips(693),
+            ),
+            BodyCellParagraph(
+                text="FX\tTelefax\tX [1P0..1]\tX [1P0..1]\tX [1P0..1]",
+                tabstop_positions=[Twips(693), Twips(3088), Twips(4064), Twips(5026)],
+                left_indent_length=Twips(693),
+            ),
+            BodyCellParagraph(
+                text="TE\tTelefon\tX [1P0..1]\tX [1P0..1]\tX [1P0..1]",
+                tabstop_positions=[Twips(693), Twips(3088), Twips(4064), Twips(5026)],
+                left_indent_length=Twips(693),
+            ),
+        ]
+
+        table = get_ahb_table_with_multiple_paragraphs(body_cell_paragraphs=body_cell_paragraphs)
+
+        indicator_tabstop_positions: list[int] = [Twips(693), Twips(3088), Twips(4064), Twips(5026)]
+
+        bc: BodyCell = BodyCell(
+            table_cell=table.row_cells(0)[0],
+            left_indent_position=left_indent_length,
+            indicator_tabstop_positions=indicator_tabstop_positions,
+        )
+
+        empty_ahb_row: pd.DataFrame = pd.DataFrame(
+            {
+                "Segment Gruppe": [""],
+                "Segment": [""],
+                "Datenelement": [""],
+                "Codes und Qualifier": [""],
+                "Beschreibung": [""],
+                "11016": [""],
+                "11017": [""],
+                "11018": [""],
+            }
+        )
+
+        df = bc.parse(ahb_row_dataframe=empty_ahb_row)
+
+        assert df.equals(expexted_df)
+
+        print()
