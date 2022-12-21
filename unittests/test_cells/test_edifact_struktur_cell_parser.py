0dd51f7e5f0a64b41b4e2da0a4234253c685b26b
kevin <kevin.krechan@hochfrequenz.de>
2022-12-21 14:30:49 +0100

    ✅Add tests for edifact struktur cells

---
+++ unittests/test_cells/test_edifact_struktur_cell_parser.py
@@ -0,0 +1,133 @@
+import pytest
+from docx.shared import Twips
+import pandas as pd
+
+from kohlrahbi.cells import EdifactStrukturCell
+from unittests.bodycellparagraph import BodyCellParagraph
+
+
+class TestEdifactStrukturCell:
+    @pytest.mark.parametrize(
+        ["edifact_struktur_cell_paragraph", "expected_dataframe"],
+        [
+            pytest.param(
+                [
+                    BodyCellParagraph(
+                        text="Nachrichten-Kopfsegment",
+                        tabstop_positions=[],
+                        left_indent_length=Twips(581),
+                    ),
+                ],
+                pd.DataFrame(
+                    {
+                        "Segment Gruppe": ["Nachrichten-Kopfsegment"],
+                        "Segment": [""],
+                        "Datenelement": [""],
+                        "Codes und Qualifier": [""],
+                        "Beschreibung": [""],
+                        "11016": [""],
+                        "11017": [""],
+                        "11018": [""],
+                        "Bedingung": [""],
+                    }
+                ),
+                id="11016 | 11017 | 11018: Nachrichten-Kopfsegment",
+            ),
+            pytest.param(
+                [
+                    BodyCellParagraph(
+                        text="UNH",
+                        tabstop_positions=[],
+                        left_indent_length=Twips(64),
+                    ),
+                ],
+                pd.DataFrame(
+                    {
+                        "Segment Gruppe": [""],
+                        "Segment": ["UNH"],
+                        "Datenelement": [""],
+                        "Codes und Qualifier": [""],
+                        "Beschreibung": [""],
+                        "11016": [""],
+                        "11017": [""],
+                        "11018": [""],
+                        "Bedingung": [""],
+                    }
+                ),
+                id="11016 | 11017 | 11018: UNH",
+            ),
+            pytest.param(
+                [
+                    BodyCellParagraph(
+                        text="UNH\t0062",
+                        tabstop_positions=[Twips(128)],  # these tabstops are made up!
+                        left_indent_length=Twips(64),
+                    ),
+                ],
+                pd.DataFrame(
+                    {
+                        "Segment Gruppe": [""],
+                        "Segment": ["UNH"],
+                        "Datenelement": ["0062"],
+                        "Codes und Qualifier": [""],
+                        "Beschreibung": [""],
+                        "11016": [""],
+                        "11017": [""],
+                        "11018": [""],
+                        "Bedingung": [""],
+                    }
+                ),
+                id="11016 | 11017 | 11018: UNH - 0062",
+            ),
+            pytest.param(
+                [
+                    BodyCellParagraph(
+                        text="SG2\tNAD\t3035",
+                        tabstop_positions=[Twips(128), Twips(256)],  # these tabstops are made up!
+                        left_indent_length=Twips(581),
+                    ),
+                ],
+                pd.DataFrame(
+                    {
+                        "Segment Gruppe": ["SG2"],
+                        "Segment": ["NAD"],
+                        "Datenelement": ["3035"],
+                        "Codes und Qualifier": [""],
+                        "Beschreibung": [""],
+                        "11016": [""],
+                        "11017": [""],
+                        "11018": [""],
+                        "Bedingung": [""],
+                    }
+                ),
+                id="11016 | 11017 | 11018: SG2 - NAD - 3035",
+            ),
+        ],
+    )
+    def test_edifact_struktur_cell_parser(
+        self, get_ahb_table_with_multiple_paragraphs, edifact_struktur_cell_paragraph, expected_dataframe
+    ):
+
+        table = get_ahb_table_with_multiple_paragraphs(body_cell_paragraphs=edifact_struktur_cell_paragraph)
+
+        esc: EdifactStrukturCell = EdifactStrukturCell(
+            table_cell=table.row_cells(0)[0], edifact_struktur_cell_left_indent_position=Twips(64)
+        )
+
+        empty_ahb_row: pd.DataFrame = pd.DataFrame(
+            {
+                "Segment Gruppe": [""],
+                "Segment": [""],
+                "Datenelement": [""],
+                "Codes und Qualifier": [""],
+                "Beschreibung": [""],
+                "11016": [""],
+                "11017": [""],
+                "11018": [""],
+                "Bedingung": [""],
+            }
+        )
+
+        df = esc.parse(ahb_row_dataframe=empty_ahb_row)
+
+        assert df.equals(expected_dataframe)
