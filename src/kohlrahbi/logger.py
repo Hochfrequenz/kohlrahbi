6ebdf0478ce2dd074d1f42ed2f6eb7a84e8d4912
kevin <kevin.krechan@hochfrequenz.de>
2022-12-27 15:20:38 +0100

    Make logger OS independent

--- src/kohlrahbi/logger.py
+++ src/kohlrahbi/logger.py
@@ -1,5 +1,9 @@
 import logging
 import logging.config
+from pathlib import Path
 
-logging.config.fileConfig("src/kohlrahbi/logging.conf")
+# Construct the path to the configuration file
+logger_config_file: Path = Path(__file__).with_suffix(".ini")
+
+logging.config.fileConfig(logger_config_file)
 logger = logging.getLogger("kohlrahbi")
