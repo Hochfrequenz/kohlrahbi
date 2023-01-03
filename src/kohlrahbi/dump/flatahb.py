530d05b2c03794a9d8cda20bce8668b6487574c6
kevin <kevin.krechan@hochfrequenz.de>
2023-01-03 16:59:43 +0100

    🚧 WIP save FlatAhb

--- src/kohlrahbi/dump/flatahb.py
+++ src/kohlrahbi/dump/flatahb.py
@@ -1,8 +1,10 @@
 import json
+import re
 from pathlib import Path
 from typing import Union
 from uuid import uuid4
 
+import maus
 import pandas as pd
 from maus.edifact import pruefidentifikator_to_format
 from maus.models.anwendungshandbuch import (
@@ -11,9 +13,18 @@ from maus.models.anwendungshandbuch import (
     FlatAnwendungshandbuch,
     FlatAnwendungshandbuchSchema,
 )
+from more_itertools import peekable
 
 from kohlrahbi.ahbtable import AhbTable
 from kohlrahbi.logger import logger
+
+_segment_group_pattern = re.compile(r"^SG\d+$")
+
+
+def get_section_name(segment_gruppe_or_section_name: str, last_section_name: str) -> str:
+    if not (segment_gruppe_or_section_name.startswith("SG") or segment_gruppe_or_section_name == ""):
+        return segment_gruppe_or_section_name
+    return last_section_name
 
 
 def convert_ahb_table_to_flatahb(ahb_table: pd.DataFrame, pruefi: str) -> FlatAnwendungshandbuch:
@@ -23,10 +34,20 @@ def convert_ahb_table_to_flatahb(ahb_table: pd.DataFrame, pruefi: str) -> FlatAn
 
     ahb_lines: list[AhbLine] = []
     index = 0
+    current_section_name: str = ""
 
-    for _, row in ahb_table.iterrows():
+    # ahb_table.to_dict(orient="index")
+
+    iterable_ahb_table = peekable(ahb_table.iterrows())
+
+    for _, row in iterable_ahb_table:
+
+        current_section_name = get_section_name(
+            segment_gruppe_or_section_name=row["Segment Gruppe"], last_section_name=current_section_name
+        )
 
         if row["Segment Gruppe"].startswith("SG"):
+            # e.g. SG2
 
             ahb_lines.append(
                 AhbLine(
@@ -37,28 +58,42 @@ def convert_ahb_table_to_flatahb(ahb_table: pd.DataFrame, pruefi: str) -> FlatAn
                     value_pool_entry=row["Codes und Qualifier"] or None,
                     name=row["Beschreibung"] or None,
                     ahb_expression=row[pruefi] or None,
-                    section_name=None,
+                    section_name=current_section_name,
                     index=index,
                 )
             )
         else:
             # e.g. Nachrichten-Kopfsegment
+
+            try:
+                # this try-except catches the case in the last iteration. The peek() method will fail there, because of the iteration end.
+                _, next_row = iterable_ahb_table.peek()
+
+                if _segment_group_pattern.match(next_row["Segment Gruppe"]):
+                    current_segment_group_key = next_row["Segment Gruppe"]
+                else:
+                    current_segment_group_key = None
+            except StopIteration:
+                current_segment_group_key = row["Segment Gruppe"]
+
             ahb_lines.append(
                 AhbLine(
                     guid=uuid4(),
-                    segment_group_key=None,
+                    segment_group_key=current_segment_group_key or None,
                     segment_code=row["Segment"] or None,
                     data_element=row["Datenelement"] or None,
                     value_pool_entry=row["Codes und Qualifier"] or None,
                     name=row["Beschreibung"] or None,
                     ahb_expression=None,
-                    section_name=row["Segment Gruppe"] or None,
+                    section_name=current_section_name,
                     index=index,
                 )
             )
         index = index + 1
 
-    return FlatAnwendungshandbuch(meta=AhbMetaInformation(pruefidentifikator=pruefi), lines=ahb_lines)
+    ahb_meta_information: AhbMetaInformation = AhbMetaInformation(pruefidentifikator=pruefi)
+
+    return FlatAnwendungshandbuch(meta=ahb_meta_information, lines=ahb_lines)
 
 
 def dump_kohlrahbi_to_flatahb(kohlrahbi: pd.DataFrame, pruefi: str, output_directory_path: Path):
@@ -83,5 +118,5 @@ def dump_kohlrahbi_to_flatahb(kohlrahbi: pd.DataFrame, pruefi: str, output_direc
 
     dump_data = FlatAnwendungshandbuchSchema().dump(flat_ahb)
 
-    with open(flatahb_output_directory_path / f"{pruefi}.json", "w") as f:
+    with open(flatahb_output_directory_path / f"{pruefi}.json", "w", encoding="utf-8") as f:
         json.dump(dump_data, f)
