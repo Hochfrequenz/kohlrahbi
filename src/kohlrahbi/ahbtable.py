323ef56707bbef33f9928e6d0a2be733eb77d557
kevin <kevin.krechan@hochfrequenz.de>
2023-01-03 16:58:22 +0100

    🎨 sanitise row

--- src/kohlrahbi/ahbtable.py
+++ src/kohlrahbi/ahbtable.py
@@ -1,10 +1,17 @@
 import attrs
 import pandas as pd
 from docx.table import Table  # type:ignore[import]
+from docx.table import _Cell
 
 from kohlrahbi.ahbtablerow import AhbTableRow
 from kohlrahbi.helper.row_type_checker import RowType, get_row_type
 from kohlrahbi.helper.seed import Seed
+
+
+def iter_visible_cells(row):
+    tr = row._tr
+    for tc in tr.tc_lst:
+        yield _Cell(tc, row.table)
 
 
 @attrs.define(auto_attribs=True, kw_only=True)
@@ -23,40 +30,11 @@ class AhbTable:
             pd.DataFrame: Returns the ahb_table_dataframe with the added information.
         """
 
-        # pylint: disable=protected-access
-        index_for_body_column = self.define_index_for_body_column()
+        for row in self.table.rows:
 
-        for row in range(len(self.table.rows)):
+            sanitized_row = list(iter_visible_cells(row=row))
 
-            row_cell_texts_as_list = [cell.text for cell in self.table.row_cells(row)]
-
-            if len(self.table.columns) == 4:
-                # remove redundant information for tables with 4 columns
-                if (
-                    row_cell_texts_as_list[0] == row_cell_texts_as_list[1]
-                    and row_cell_texts_as_list[2] == row_cell_texts_as_list[3]
-                ):
-                    # pylint: disable=line-too-long
-                    # HEADER looks like
-                    # 0:'EDIFACT Struktur'
-                    # 1:'EDIFACT Struktur'
-                    # 2:'Beschreibung\tKündigung\tBestätigung\tAblehnung\tBedingung\n\tMSB \tKündigung\tKündigung\n\tMSB \tMSB \nKommunikation von\tMSBN an\tMSBA an\tMSBA an\n\tMSBA\tMSBN\tMSBN\nPrüfidentifikator\t11039\t11040\t11041'
-                    # 3:'Beschreibung\tKündigung\tBestätigung\tAblehnung\tBedingung\n\tMSB \tKündigung\tKündigung\n\tMSB \tMSB \nKommunikation von\tMSBN an\tMSBA an\tMSBA an\n\tMSBA\tMSBN\tMSBN\nPrüfidentifikator\t11039\t11040\t11041'
-                    # len():4
-                    del row_cell_texts_as_list[1]
-                    row_cell_texts_as_list[2] = ""
-                elif row_cell_texts_as_list[1] == row_cell_texts_as_list[2]:
-                    # Dataelement row with header in the table
-                    # 0:'SG2\tNAD\t3035'
-                    # 1:'SG2\tNAD\t3035'
-                    # 2:'MR\tNachrichtenempfänger\tX\tX\tX'
-                    # 3:''
-                    # len():4
-                    del row_cell_texts_as_list[1]
-                elif row_cell_texts_as_list[0] == row_cell_texts_as_list[1]:
-                    del row_cell_texts_as_list[1]
-
-            current_edifact_struktur_cell = self.table.row_cells(row)[0]
+            current_edifact_struktur_cell = sanitized_row[0]
 
             # check for row type
             current_row_type = get_row_type(
@@ -64,9 +42,9 @@ class AhbTable:
                 left_indent_position=self.seed.edifact_struktur_left_indent_position,
             )
 
-            edifact_struktur_cell = self.table.row_cells(row)[0]
-            middle_cell = self.table.row_cells(row)[index_for_body_column]
-            bedingung_cell = self.table.row_cells(row)[-1]
+            edifact_struktur_cell = sanitized_row[0]
+            middle_cell = sanitized_row[1]
+            bedingung_cell = sanitized_row[-1]
 
             # this case covers the "normal" docx table row
             if not (current_row_type is RowType.EMPTY and self.seed.last_two_row_types[0] is RowType.HEADER):
@@ -98,13 +76,6 @@ class AhbTable:
             self.seed.last_two_row_types[0] = current_row_type
 
         return ahb_table_dataframe
-
-    def define_index_for_body_column(self) -> int:
-        """ """
-        if self.table._column_count == 4:
-            return 2
-        else:
-            return 1
 
     @staticmethod
     def fill_segement_gruppe_segement_dataelement(df: pd.DataFrame):
