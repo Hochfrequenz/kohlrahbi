7da3c46d871efb6888787aa9f60c677d376235e5
kevin <kevin.krechan@hochfrequenz.de>
2023-02-02 20:09:51 +0100

    ✅ Add unit test for AhbSubTable

---
+++ unittests/test_ahb_sub_table.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import docx
+import pytest  # type: ignore[import]
+from docx.table import Table
+
+from kohlrahbi.ahbsubtable import AhbSubTable
+from kohlrahbi.helper.read_functions import get_all_paragraphs_and_tables
+
+
+class TestAhbSubTable:
+    """
+    All tests regarding the AhbSubTable class
+    """
+
+    def test_from_table(self):
+        """
+        Test the instantiation of a AhbSubTable
+        """
+        ahb_file_path: Path = Path.cwd() / Path("unittests/docx_files/UTILMD-11042-test.docx")
+        doc = docx.Document(ahb_file_path)  # Creating word reader object.
+        all_paragraphs_and_tables = list(get_all_paragraphs_and_tables(parent=doc))
+
+        docx_table = all_paragraphs_and_tables[
+            1
+        ]  # the first item in a word file seems to be always a paragraph. So we need the second item.
+
+        if isinstance(docx_table, Table):
+            ahb_sub_table = AhbSubTable.from_table(docx_table=docx_table)
+
+            assert isinstance(ahb_sub_table, AhbSubTable)
+        else:
+            raise TypeError("You did not pass a docx table instance.")
