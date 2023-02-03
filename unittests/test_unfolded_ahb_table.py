f4e351b135e881cbfee1f0a37d9e072344382a6c
kevin <kevin.krechan@hochfrequenz.de>
2023-02-03 23:53:42 +0100

    ✅ Add base for the unittests for unfolded ahb table

---
+++ unittests/test_unfolded_ahb_table.py
@@ -0,0 +1,36 @@
+import pytest
+
+from kohlrahbi.unfoldedahbtable import UnfoldedAhb
+
+
+class TestUnfoldedAhbTable:
+    """
+    All tests regarding the AhbTable class
+    """
+
+    def test_from_ahb_table(self):
+        pass
+
+    def test_get_section_name(self):
+        pass
+
+    def test_is_section_name(self):
+        pass
+
+    def test_is_segment_group(self):
+        pass
+
+    def test_is_segment_opening_line(self):
+        pass
+
+    def test_is_just_segment(self):
+        pass
+
+    def test_is_dataelement(self):
+        pass
+
+    def test_convert_to_flat_ahb(self):
+        pass
+
+    def test_convert_to_dataframe(self):
+        pass
