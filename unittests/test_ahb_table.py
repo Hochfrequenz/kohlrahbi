b28ba0f0341b1a91943b19f540a803f7c27759ea
kevin <kevin.krechan@hochfrequenz.de>
2023-02-14 11:21:05 +0100

    ✅ Add test class for AhbTable

--- unittests/test_ahb_table.py
+++ unittests/test_ahb_table.py
@@ -1,5 +1,8 @@
 import attrs
-import pytest
+import pandas as pd
+import pytest  # type:ignore[import]
+
+from kohlrahbi.ahb.ahbtable import AhbTable
 
 
 @attrs.define(auto_attribs=True, kw_only=True)
@@ -12,3 +15,46 @@ class TestAhbTable:
         """
         Test appending of an AHB subtable
         """
+
+    @pytest.mark.parametrize(
+        "ahb_table_dataframe, expected_ahb_table_dataframe",
+        [
+            pytest.param(
+                pd.DataFrame(
+                    {
+                        "Segment Gruppe": ["SG8", "Referenz auf die ID einer", "Messlokation", "SG8"],
+                        "Segment": ["SEQ", "", "", ""],
+                        "Datenelement": ["1229", "", "", ""],
+                        "Codes und Qualifier": ["Z50", "", "", ""],
+                        "Beschreibung": ["Messdatenregistriergerätedaten", "", "", ""],
+                        "11042": ["", "", "", ""],
+                        "11043": ["X", "", "", ""],
+                        "11044": ["", "", "", ""],
+                        "Bedingung": ["", "", "", ""],
+                    }
+                ),
+                pd.DataFrame(
+                    {
+                        "Segment Gruppe": ["SG8", "Referenz auf die ID einer Messlokation", "SG8"],
+                        "Segment": ["SEQ", "", ""],
+                        "Datenelement": ["1229", "", ""],
+                        "Codes und Qualifier": ["Z50", "", ""],
+                        "Beschreibung": ["Messdatenregistriergerätedaten", "", ""],
+                        "11042": ["", "", ""],
+                        "11043": ["X", "", ""],
+                        "11044": ["", "", ""],
+                        "Bedingung": ["", "", ""],
+                    }
+                ),
+            )
+        ],
+    )
+    def test_sanitize_ahb_table_dataframe(self, ahb_table_dataframe, expected_ahb_table_dataframe):
+        """ """
+
+        ahb_table = AhbTable(table=ahb_table_dataframe)
+
+        ahb_table.sanitize()
+
+        assert len(ahb_table.table) == len(expected_ahb_table_dataframe)
+        assert ahb_table.table.equals(expected_ahb_table_dataframe)
