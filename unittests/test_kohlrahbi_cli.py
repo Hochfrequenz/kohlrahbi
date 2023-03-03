22ae24c3223a8f8999f4ffe3c8f0fac9e6b4679f
kevin <kevin.krechan@hochfrequenz.de>
2023-03-03 17:05:47 +0100

    ✅ Test assume-yes flag

--- unittests/test_kohlrahbi_cli.py
+++ unittests/test_kohlrahbi_cli.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 import pytest  # type:ignore[import]
@@ -35,7 +36,7 @@ class TestCli:
             ),
         ],
     )
-    def test_harvester_cli_with_invalid_arguments(
+    def test_kohlrahbi_cli_with_invalid_arguments(
         self, datafiles, pruefis, input_folder_name: str, output_folder_name: str, expected_response
     ):
         input_path: Path = Path(datafiles) / Path(input_folder_name)
@@ -60,34 +61,53 @@ class TestCli:
         "./unittests/docx_files/UTILMDAHBWiM-informatorischeLesefassung3.1eKonsolidierteLesefassungmitFehlerkorrekturenStand25.10.2022_20230930_20221025.docx"
     )
     @pytest.mark.parametrize(
-        "pruefis, input_folder_name, output_folder_name, expected_response",
+        "argument_options, input_folder_name, output_folder_name, expected_response",
         [
             pytest.param(
-                "11016",
-                "",
-                "",
+                [
+                    "-p",
+                    "11016",
+                    "--file-type",
+                    "csv",
+                ],
+                "",  # if the folder name is empty, the path will point on the temporary directory which is created by datafiles -> valid path
+                "",  # if the folder name is empty, the path will point on the temporary directory which is created by datafiles -> valid path
                 {"exit_code": 0, "output_snippet": ""},
                 id="proof of concept",
             ),
+            pytest.param(
+                [
+                    "-p",
+                    "11016",
+                    "--file-type",
+                    "csv",
+                    "-y",
+                ],
+                "",  # if the folder name is empty, the path will point on the temporary directory which is created by datafiles -> valid path
+                "",  # if the folder name is empty, the path will point on the temporary directory which is created by datafiles -> valid path
+                {"exit_code": 0, "output_snippet": ""},
+                id="test assume yes",
+            ),
         ],
     )
-    def test_harvester_cli_with_valid_arguments(
-        self, datafiles, pruefis, input_folder_name: str, output_folder_name: str, expected_response
+    def test_kohlrahbi_cli_with_valid_arguments(
+        self, datafiles, input_folder_name, output_folder_name, argument_options, expected_response
     ):
+        """
+        This test runs the CLI tool with valid arguments and checks if the output is as expected.
+        """
+
         input_path: Path = Path(datafiles) / Path(input_folder_name)
         output_path: Path = Path(datafiles) / Path(output_folder_name)
 
-        argument_options: list[str] = [
-            "--pruefis",
-            pruefis,
-            "--input_path",
-            str(input_path),
-            "--output_path",
-            str(output_path),
-        ]
+        argument_options.extend(["--input_path", str(input_path), "--output_path", str(output_path)])
 
         # Call the CLI tool with the desired arguments
         response: Result = runner.invoke(main, argument_options)
 
         assert response.exit_code == expected_response.get("exit_code")
         assert expected_response.get("output_snippet") in response.output
+
+        path_to_new_fancy_folder = Path("./output/new_and_fancy")
+        if path_to_new_fancy_folder.exists() and path_to_new_fancy_folder.is_dir():
+            shutil.rmtree(path_to_new_fancy_folder)
