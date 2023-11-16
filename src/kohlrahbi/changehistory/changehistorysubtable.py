a02f167a0e82c607db404542852c3f89a94bd483
hf-krechan <kevin.krechan@hochfrequenz.de>
2023-11-16 12:05:06 +0100

    🎨 Add new classes changehistorytable and changehistorysubtable

---
+++ src/kohlrahbi/changehistory/changehistorysubtable.py
@@ -0,0 +1,11 @@
+import attrs
+import pandas as pd
+
+
+@attrs.define(auto_attribs=True, kw_only=True)
+class ChangeHistorySubTable:
+    """
+    This class  contains the change history table.
+    """
+
+    table: pd.DataFrame
---
+++ src/kohlrahbi/changehistory/changehistorytable.py
@@ -0,0 +1,11 @@
+import attrs
+import pandas as pd
+
+
+@attrs.define(auto_attribs=True, kw_only=True)
+class ChangeHistoryTable:
+    """
+    This class  contains the change history table.
+    """
+
+    table: pd.DataFrame
