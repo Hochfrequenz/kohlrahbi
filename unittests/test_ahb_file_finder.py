226630a49cf4d073b1ca61775447ff4898318a9a
hf-krechan <kevin.krechan@hochfrequenz.de>
2023-12-14 10:56:54 +0100

    ✅ Update unit test

--- unittests/test_ahb_file_finder.py
+++ unittests/test_ahb_file_finder.py
@@ -41,11 +41,13 @@ class TestDocxFileFinder:
     def test_filter_docx_files_for_edifact_format(self):
         """
         Test the filter_docx_files_for_edifact_format method.
-        This method filters the list of AHB docx paths for the given EDIFACT format.
-        But it does not filter for the latest AHB docx files.
-        So we assert two ahb files for the MSCONS format:
-        - MSCONSAHB-informatorischeLesefassung3.1a_20230331_20221001
+        This method filters the list of docx paths for the given EDIFACT format.
+        But it does not filter for the latest AHB docx files neither does it filter for AHBs or MIGs only.
+        So we assert two ahb files and two MIG files for the MSCONS format:
         - MSCONSAHB-informatorischeLesefassung3.1aKonsolidierteLesefassungmitFehlerkorrekturenStand27.09.2022_20230331_20221001
+        - MSCONSMIG-informatorischeLesefassung2.4a_99991231_20221001
+        - MSCONSAHB-informatorischeLesefassung3.1a_20230331_20221001
+        - MSCONSMIG-informatorischeLesefassung2.4aKonsolidierteLesefassungmitFehlerkorrekturenStand23.05.2022_99991231_20221001
         """
         path_to_ahb_documents: Path = Path.cwd() / Path("unittests/docx_files")
 
@@ -53,7 +55,7 @@ class TestDocxFileFinder:
 
         ahb_file_finder.filter_docx_files_for_edifact_format(edifact_format=EdifactFormat.MSCONS)
 
-        assert len(ahb_file_finder.paths_to_docx_files) == 2
+        assert len(ahb_file_finder.paths_to_docx_files) == 4
 
     def test_filter_for_latest_ahb_docx_files(self):
         """
