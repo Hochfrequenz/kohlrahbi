958bb063e2e79ee30d93262bb6afea9a792729e9
hf-krechan <kevin.krechan@hochfrequenz.de>
2024-03-30 21:22:27 +0100

    📝 add documentation

--- unittests/test_ahb_sub_table.py
+++ unittests/test_ahb_sub_table.py
@@ -1,3 +1,7 @@
+"""
+This module contains all tests regarding the AhbSubTable class
+"""
+
 from pathlib import Path
 
 import docx  # type:ignore[import]
@@ -15,14 +19,19 @@ class TestAhbSubTable:
     def test_from_table(self) -> None:
         """
         Test the instantiation of a AhbSubTable
+
+        This method tests the instantiation of an AhbSubTable object by creating a word reader object,
+        extracting the table from the document, and checking if the resulting object is an instance of AhbSubTable.
+
+        Raises:
+            TypeError: If the input is not a docx table instance.
         """
         ahb_file_path: Path = Path.cwd() / Path("unittests/test-files/docx_files/UTILMD-11042-test.docx")
-        doc = docx.Document(ahb_file_path)  # Creating word reader object.
+        doc = docx.Document(str(ahb_file_path))  # Creating word reader object.
         all_paragraphs_and_tables = list(get_all_paragraphs_and_tables(parent=doc))
 
-        docx_table = all_paragraphs_and_tables[
-            1
-        ]  # the first item in a word file seems to be always a paragraph. So we need the second item.
+        # the first item in a word file seems to be always a paragraph. So we need the second item.
+        docx_table = all_paragraphs_and_tables[1]
 
         if isinstance(docx_table, Table):
             ahb_sub_table = AhbSubTable.from_table_with_header(docx_table=docx_table)
