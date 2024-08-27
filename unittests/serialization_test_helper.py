f5e49050b8c4cef1ccab28fbff9e4b6bd909de08
kevin <kevin.krechan@hochfrequenz.de>
2024-08-27 10:11:31 +0200

    🚧 WIP

--- unittests/serialization_test_helper.py
+++ unittests/serialization_test_helper.py
@@ -1,22 +1,33 @@
 import json
 from typing import TypeVar
 
-T = TypeVar("T")
+from deepdiff import DeepDiff
+from pydantic import BaseModel
+
+T = TypeVar("T", bound=BaseModel)
 
 
-def assert_serialization_roundtrip(serializable_object: T, schema: Schema, expected_json_dict: dict) -> T:
+def assert_serialization_roundtrip(serializable_object: T, expected_json_dict: dict) -> T:
     """
-    Serializes the serializable_object using the provided schema,
+    Serializes the serializable_object,
     asserts, that the result is equal to the expected_json_dict
     then deserializes it again and asserts on equality with the original serializable_object
 
     :returns the deserialized_object
     """
-    json_string = schema.dumps(serializable_object)
+    json_string = serializable_object.model_dump_json(exclude_unset=True)
     assert json_string is not None
-    actual_json_dict = json.loads(json_string)
-    assert actual_json_dict == expected_json_dict
-    deserialized_object = schema.loads(json_data=json_string)
+
+    expected_object = serializable_object.__class__.model_validate(expected_json_dict)
+
+    serializable_object_dict = serializable_object.model_dump(exclude_unset=True, exclude_defaults=True, mode="json")
+    expected_object_dict = expected_object.model_dump(exclude_unset=True, exclude_defaults=True, mode="json")
+
+    diff = DeepDiff(serializable_object_dict, expected_object_dict)
+
+    assert serializable_object_dict == expected_object_dict
+
+    deserialized_object = serializable_object.model_validate_json(json_string)
     assert isinstance(deserialized_object, type(serializable_object))
     assert deserialized_object == serializable_object
     return deserialized_object
