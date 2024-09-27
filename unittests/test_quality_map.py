25a307d16d4256de7d8350e8c2f1867e147c0076
kevin <kevin.krechan@hochfrequenz.de>
2024-09-27 06:33:55 +0200

    add snapshot test

--- unittests/test_quality_map.py
+++ unittests/test_quality_map.py
@@ -1,3 +1,4 @@
+import glob
 from pathlib import Path
 from typing import Union
 from unittest.mock import MagicMock
@@ -67,11 +68,11 @@ class TestQualityMap:
 
         argument_options.extend(
             [
+                "--assume-yes",
                 "--edi-energy-mirror-path",
                 str(Path(__file__).parents[1] / "edi_energy_mirror"),
                 "--output-path",
                 str(actual_output_dir),
-                "--assume-yes",
             ]
         )
 
@@ -79,15 +80,17 @@ class TestQualityMap:
         response: Result = runner.invoke(cli, argument_options)
 
         assert response.exit_code == expected_response.get("exit_code")
-        expected_output_snippet = expected_response.get("output_snippet")
-        if expected_output_snippet is not None and isinstance(expected_output_snippet, str):
-            assert expected_output_snippet in response.output
-        else:
-            assert False  # break the test if the output_snippet is None
 
         # Check if the generated files are the same as the expected files
 
-        path_to_actual_csv_file = get_csv_paths(actual_output_dir)
+        def get_csv_file(directory: Path) -> Path:
+            csv_files = glob.glob(str(directory / "*.csv"))
+            if csv_files:
+                return Path(csv_files[0])
+            else:
+                raise FileNotFoundError("No CSV file found in the given directory")
 
-        with open(path_to_actual_csv_file[0], "r", encoding="utf-8") as actual_csv:
+        path_to_actual_csv_file = get_csv_file(actual_output_dir)
+
+        with open(path_to_actual_csv_file, "r", encoding="utf-8") as actual_csv:
             assert snapshot == actual_csv.read()
